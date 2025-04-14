adec84750c187e22cf0d380886fa2548cc0c3c94
jose tomas ruiz <j_tomas_93@hotmail.com>
2025-04-14 19:02:16 -0400

    integracion de coingkeco

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hola Mundo");
+async function getCrypto() {
+  const response = await fetch('https://api.coingecko.com/api/v3/coins/list');
+  const data = await response.json();
+  console.log(data);
+}
+getCrypto();
